e9e2224a75423f868940e9c36f495a79f14be2af
JEFFREY BURKHOLDER 0887479 <jeffrey.burkholder@cvshealth.com>
2018-03-27 13:32:00 -0500

    added 108 hiddden program for SMS STOP

--- AlertSubscribeDeploy/CreateECCM_MemberCommunicationProgram.sql
+++ AlertSubscribeDeploy/CreateECCM_MemberCommunicationProgram.sql
@@ -19,12 +19,13 @@ INSERT INTO ECCM.TMCP_MMR_CMM_PGM
    (MCP_MMR_CMM_PGM_ID, MCP_CMM_PGM_NM ,MCP_CMM_PGM_DSC,
     MCP_HSC_TRN_CD, MCP_HSC_SRC_CD, MCP_HSC_USR_ID, MCP_HSC_TS,
     MCP_HSU_TRN_CD, MCP_HSU_SRC_CD, MCP_HSU_USR_ID, MCP_HSU_TS ) VALUES
-   (101, 'Full Drug Name',                 'Full Drug Name Description',                 0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
-   (102, 'Mail Order Status',              'Mail Order Status Description',              0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
-   (103, 'Initial Mail Refill Reminder',   'Initial Mail Refill Reminder Description',   0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
-   (104, 'Followup Mail Refill Reminders', 'Followup Mail Refill Reminders Description', 0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
-   (105, 'Payment Notifications',          'Payment Notifications Description',          0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
-   (106, 'ADT Alerts?',                    'ADT Alerts? Description',                    0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
-   (107, 'RFM',                            'RFM Description',                            0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP);
+   (101, 'Full Drug Name',                 'Recieve full drug name within your digital communications',    0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
+   (102, 'Mail Order Status',              'Orders status updates about on your mail order prescriptions', 0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
+   (103, 'Initial Mail Refill Reminder',   'First refill reminder for your eligible prescription',         0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
+   (104, 'Followup Mail Refill Reminders', 'Additional refill reminders following initial reminder',       0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
+   (105, 'Payment Notifications',          'Payment and credit card expiration communications',            0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
+   (106, 'Drug Therapy Alerts',            'Adherence to drug therapy alerts',                             0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
+   (107, 'RFM',                            'Mandatory alerts associated with ready fill at mail program',  0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP),
+   (108, 'Hidden Program',                 'Hidden program to store core ECCM system rules',               0, 0, 'QCPI24K', CURRENT_TIMESTAMP, 0, 0, 'QCPI24K', CURRENT_TIMESTAMP);
    
 SELECT * FROM ECCM.TMCP_MMR_CMM_PGM;   
